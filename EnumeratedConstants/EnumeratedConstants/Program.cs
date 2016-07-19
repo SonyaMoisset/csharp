11c27745fcc549492c81857955b29c87f90bbf90
SonyaMoisset <sonya.moisset@gmail.com>
2016-07-19 21:40:47 +0100

    C# EnumeratedConstants
    C# EnumeratedConstants

---
+++ EnumeratedConstants/EnumeratedConstants/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumeratedConstants
+{
+    class Program
+    {
+        enum Color
+        {
+            Red, 
+            Orange,
+            Green,
+            Blue,
+            Indigo,
+            Violet
+        }
+
+        static void Main(string[] args)
+        {
+            Color favorite = Color.Green;
+
+            switch(favorite)
+            {
+                case Color.Red:
+                    Console.WriteLine("You chose Red");
+                    break;
+                case Color.Orange:
+                    Console.WriteLine("You chose Orange");
+                    break;
+                case Color.Green:
+                    Console.WriteLine("You chose Green");
+                    break;
+                case Color.Blue:
+                    Console.WriteLine("You chose Blue");
+                    break;
+                case Color.Indigo:
+                    Console.WriteLine("You chose Indigo");
+                    break;
+                case Color.Violet:
+                    Console.WriteLine("You chose Violet");
+                    break;
+                default:
+                    Console.WriteLine("You didn't choose a color");
+                    break;
+
+
+            }
+        }
+    }
+}
