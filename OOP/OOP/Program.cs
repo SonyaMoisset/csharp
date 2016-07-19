e1e77142a38beab6cc6ddf9a779d14ff1cd70467
SonyaMoisset <sonya.moisset@gmail.com>
2016-07-19 23:43:48 +0100

    C# OOP
    C# OOP

---
+++ OOP/OOP/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var myComputer = new Computer();
+            //myComputer.ID = 102;
+
+            //Item mySecondComputer = new Computer();
+            //mySecondComputer.ID = 103;
+
+            //Computer mySecondComputer2 = mySecondComputer as Computer;
+            //mySecondComputer2.CPUType = "i7";
+            //string myCPUType = mySecondComputer2.CPUType;
+            //mySecondComputer2.Name = "Jupiter";
+
+            //mySecondComputer2.Purchase();
+
+            //Software mySoftware = new Software();
+            //mySoftware.Name = "Foo";
+            //mySoftware.ID = 101;
+            //mySoftware.ISBN = "1234";
+            //mySoftware.Purchase();
+
+            Item newItem = Item.GetItem();
+            Console.WriteLine("New item ID = {0}, name = {1}", newItem.ID, newItem.Name);
+
+
+        }
+    }
+}
